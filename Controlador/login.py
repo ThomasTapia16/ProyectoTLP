3e202f83c151a4860fe400b0b648f7b866adea8f
ThomasTapia16 <thomas@git.com>
2021-11-25 02:45:32 -0300

    loggin

---
+++ Controlador/login.py
@@ -0,0 +1,18 @@
+from django.core.checks import messages
+from django.shortcuts import render
+from Modelo.models import Funcionario
+
+
+def logging(request):
+
+    if request.method == 'POST':
+        
+        try:
+            datosFuncionario = Funcionario.objects.get(usuario = request.POST['username'], contrasena = request.POST['pwd'])
+            print("usuario",datosFuncionario)
+            request.session['usuario'] = datosFuncionario.usuario
+            return render(request,'login/home.html')
+        except datosFuncionario.DoesNotExist as e:
+            messages.succes(request,'Datos incorrectos aweonao culiao...')
+    
+    return render(request,'login/login.html')
