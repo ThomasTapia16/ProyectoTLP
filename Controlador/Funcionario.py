f88c81fe83825fd4de96cd360e5eba35a653be2f
ThomasTapia16 <thomas@git.com>
2021-11-25 00:39:11 -0300

    Controlador

---
+++ Controlador/Funcionario.py
@@ -0,0 +1,77 @@
+class Funcionario:
+
+    def __init__(self,rut,nombre,contraseña):
+        self.rut = rut
+        self.nombre = nombre
+        self.contraseña = contraseña
+
+    
+    def registrarUsuario():
+        pass
+
+
+    # def dejarBicicleta():
+    #     pass
+    
+    # def retirarBicicleta():
+    #     pass
+
+    # def solicitarElementos():
+    #     pass
+    
+    
+    
+    # def registrarIncidente():
+    #     pass
+    
+    # def actualizarElementos():
+    #     pass
+    
+    # def agregarElementos():
+    #     pass
+
+    # def reservarElemento():
+    #     pass
+    
+    # def pagarEstacionamiento():
+    #     pass
+    # def utilizarReserva():
+    #     pass
+    
+    # def crearServicio():#aqui va un parametro
+    #     pass
+
+    # def revisaServicio():#aqui va un parametro
+    #     pass
+
+    # def mostrarServicio():#aqui va un parametro
+    #     pass
+
+    # def terminarServicio():#aqui va un parametro
+    #     pass
+
+    # def modificarServicio():#aqui va un parametro
+    #     pass
+
+    # def cargarServicio():#aqui va un parametro
+    #     pass
+
+    # def recibeElementos():#aqui va un parametro
+    #     pass
+    
+    # def ingresarElemento():
+    #     pass
+
+    # def crearElemento():#aqui va un parametro
+    #     pass
+
+    # def solicitarTelefonoCliente():
+    #     pass
+
+    # def emitirReporte():
+    #     pass
+
+    # def darDeBaja():
+    #     pass
+    # def registrarPago():#aqui va un parametro
+    #     pass
